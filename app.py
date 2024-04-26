8c8762a588f71331836e795d77bf024ec1ff88b3
hukerikar <srujanhukerikar21@gmail.com>
2024-04-26 20:45:08 +0530

    Update app.py
    a

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/hr_interview')
+def hr_interview():
+    return render_template('hr_interview.html')
+
+@app.route('/technical_interview')
+def technical_interview():
+    return render_template('technical_interview.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
